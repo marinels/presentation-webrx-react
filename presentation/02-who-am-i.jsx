57af125eccdfbc4c6442cc58caea9a157b6b999a
Pat Sissons <patricksissons@gmail.com>
2017-06-18 00:56:26 -0700

    fixing browser errors

--- presentation/02-who-am-i.jsx
+++ presentation/02-who-am-i.jsx
@@ -2,12 +2,12 @@ import { React, Tags, Images } from './slide';
 
 const renderGitHubLink = (image, name) => {
   return (
-    <Tags.Text style={ ({ display: 'table-row' }) }>
+    <div style={ ({ display: 'table-row' }) }>
       <Tags.Image className='img-circle profile' display='table-cell' margin='1rem' height={ 144 } src={ image } />
       <Tags.Heading size={ 4 } textAlign='left' margin='1rem' lineHeight='144px' style={ ({ display: 'table-cell', verticalAlign: 'middle' }) }>
         <Tags.Link href={ `https://github.com/${ name }` }><i className='fa fa-github' />{ `/${ name }` }</Tags.Link>
       </Tags.Heading>
-    </Tags.Text>
+    </div>
   );
 };
 
@@ -17,9 +17,9 @@ export default (
     <Tags.Heading size={ 4 }>Pat Sissons</Tags.Heading>
     <Tags.Heading size={ 4 }>Senior Software Developer</Tags.Heading>
     <Tags.Heading size={ 4 }>@ Marine Learning Systems</Tags.Heading>
-    <Tags.Text textAlign='center' style={ ({ display: 'table' }) }>
+    <div style={ ({ display: 'table', textAlign: 'center', margin: 'auto' }) }>
       { renderGitHubLink(Images.me, 'patsissons') }
       { renderGitHubLink(Images.logo, 'marinels') }
-    </Tags.Text>
+    </div>
   </Tags.Slide>
 );
