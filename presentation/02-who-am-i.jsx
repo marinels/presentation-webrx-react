f3e361ebffc0caac7fda23cf6aac2d7662209f5b
Pat Sissons <patricksissons@gmail.com>
2017-06-17 22:44:36 -0700

    improving the who am i slide
    simplification

--- presentation/02-who-am-i.jsx
+++ presentation/02-who-am-i.jsx
@@ -1,21 +1,25 @@
 import { React, Tags, Images } from './slide';
 
+function renderGitHubLink(image, name) {
+  return (
+    <Tags.Text style={ ({ display: 'table-row' }) }>
+      <Tags.Image className='img-circle profile' display='table-cell' margin='1rem' height={ 144 } src={ image } />
+      <Tags.Heading size={ 4 } textAlign='left' margin='1rem' lineHeight='144px' style={ ({ display: 'table-cell', verticalAlign: 'middle' }) }>
+        <Tags.Link href={ `https://github.com/${ name }` }><i className='fa fa-github' />{ `/${ name }` }</Tags.Link>
+      </Tags.Heading>
+    </Tags.Text>
+  );
+}
+
 export default (
   <Tags.Slide>
     <Tags.Heading>Who Am I?</Tags.Heading>
     <Tags.Heading size={ 4 }>Pat Sissons</Tags.Heading>
     <Tags.Heading size={ 4 }>Senior Software Developer</Tags.Heading>
     <Tags.Heading size={ 4 }>@ Marine Learning Systems</Tags.Heading>
-    <span>
-      <Tags.Image className='img-circle img-center profile' height={ 144 } src={ images.me } />
-      <Tags.Image className='img-circle img-center profile' height={ 144 } src={ images.logo } />
-    </span>
-    <br />
-    <Tags.Heading size={ 5 }>
-      <Tags.Link href='https://github.com/marinels'><i className='fa fa-github' />/marinels</Tags.Link>
-    </Tags.Heading>
-    <Tags.Heading size={ 5 }>
-      <Tags.Link href='https://github.com/patsissons'><i className='fa fa-github' />/patsissons</Tags.Link>
-    </Tags.Heading>
+    <Tags.Text textAlign='center' style={ ({ display: 'table' }) }>
+      { renderGitHubLink(Images.me, 'patsissons') }
+      { renderGitHubLink(Images.logo, 'marinels') }
+    </Tags.Text>
   </Tags.Slide>
 );
