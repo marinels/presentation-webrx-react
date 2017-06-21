72cd2d15687998256612b5b0325916a0012c22c6
Pat Sissons <patricksissons@gmail.com>
2017-06-21 00:24:28 -0700

    adding comment

--- slides/slide-rxjs-demo.tsx
+++ slides/slide-rxjs-demo.tsx
@@ -6,6 +6,7 @@ const baseUri = '//baconipsum.com/api/?type=all-meat&format=html&';
 Rx.Observable
   .timer(0, 5000)
   .take(10)
+  // pick a random number of paragraphs between 1 and 5
   .map(x => Math.floor(Math.random() * 5) + 1)
   .flatMap(x => {
     return Rx.Observable
