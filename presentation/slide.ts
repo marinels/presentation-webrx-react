c60030fb1b829c65619877da4365885de6985df2
Pat Sissons <patricksissons@gmail.com>
2017-06-21 00:22:23 -0700

    increasing the default slide max width

--- presentation/slide.ts
+++ presentation/slide.ts
@@ -1,4 +1,7 @@
 import * as React from 'react';
 import * as Tags from 'spectacle';
 
+// increase the default max width a bit
+(<any>Tags.Slide).defaultProps.maxWidth = 1280;
+
 export { React, Tags };
