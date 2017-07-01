252e1def4f3b82135747111c9151aa8a352e58fe
Pat Sissons <patricksissons@gmail.com>
2017-07-01 12:10:45 -0700

    removing unnecessary IGNORE_LINE's from declarations file

--- examples/declarations.ts
+++ examples/declarations.ts
@@ -1,3 +1,3 @@
-export let Rx: any; // IGNORE_LINE
-export let Observable: any; // IGNORE_LINE
-export let wx: any; // IGNORE_LINE
+export let Rx: any;
+export let Observable: any;
+export let wx: any;
