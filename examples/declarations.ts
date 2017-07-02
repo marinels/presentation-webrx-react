b69574c0dcb58605b3863cfeb6989373009aaf64
Pat Sissons <patricksissons@gmail.com>
2017-07-01 17:39:07 -0700

    improving the type decls for wx

--- examples/declarations.ts
+++ examples/declarations.ts
@@ -1,3 +1,9 @@
+interface WxStatic {
+  command<T>(p?: (x: any) => T): any;
+  property<T>(v?: T): any;
+  whenAny(...args: any[]): any;
+}
+
 export let Rx: any;
 export let Observable: any;
-export let wx: any;
+export let wx: WxStatic;
