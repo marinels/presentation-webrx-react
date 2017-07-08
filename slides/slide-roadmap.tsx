2997fd62459152cbfdf9baecab61f536a3dd32be
Pat Sissons <patricksissons@gmail.com>
2017-07-08 14:40:22 -0700

    removing rxjs 5 from roadmap it's already in the relaesed version

--- slides/slide-roadmap.tsx
+++ slides/slide-roadmap.tsx
@@ -6,7 +6,6 @@ export = (
       { 'Roadmap' }
     </Tags.Heading>
     <br />
-    <Tags.Text>RxJS 5 (<Tags.Code>develop</Tags.Code>)</Tags.Text>
     <Tags.Text>First Official Non-Beta Release</Tags.Text>
     <Tags.Text>Component Abstractions (e.g., Material)</Tags.Text>
   </Tags.Slide>
