61b984168105ee04c2245d3933d5b44cc1011dff
Pat Sissons <patricksissons@gmail.com>
2017-07-11 21:00:10 -0700

    fixing some fonts on two code panes this will remove the scroll bar at the bottom

--- slides/slide-bindableinput-commandbutton.tsx
+++ slides/slide-bindableinput-commandbutton.tsx
@@ -7,6 +7,6 @@ export = (
     <Tags.Heading size={ 4 }>
       <Tags.Text><Tags.Code>BindableInput</Tags.Code> & <Tags.Code>CommandButton</Tags.Code></Tags.Text>
     </Tags.Heading>
-    <Tags.CodePane lang='jsx' source={ source } textSize='1.5rem' style={ ({ lineHeight: 'normal' }) } />
+    <Tags.CodePane lang='jsx' source={ source } textSize='1.4rem' style={ ({ lineHeight: 'normal' }) } />
   </Tags.Slide>
 );
